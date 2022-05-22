648993c91c8d71622ceb840a63c541a7d0d277ec
Archie Wood <archiewood@outlook.com>
2022-05-21 21:51:26 -0400

    UI updates

--- tcx.py
+++ tcx.py
@@ -3,43 +3,39 @@ import numpy as np
 import pandas as pd
 from datetime import datetime, timedelta
 import plotly.express as px
+import plotly.graph_objects as go
 import random
 
 from tcx_decoder import get_dataframes
 from interpolate_trackpoints import interpolate_trackpoints
 
-st.header('TCX file fixer')
+'# TCX File Fixer'
 
-st.write('Upload tcx files with dodgy gps data and fix it for Strava.')
-st.write('This will also fix (to some extent) any issues with erroneous pace and segments.')
+st.write('Upload .tcx files from [Garmin](https://connect.garmin.com/modern/activities) with dodgy GPS data and fix it for Strava.')
+st.write('This should also (to some extent) fix issues with erroneous pace.')
 
 tcx_file=st.file_uploader('Upload a tcx file', type=['tcx'], accept_multiple_files=False, key=None, help='You can download the file from Garmin connect or Strava')
+
+
 if tcx_file is not None:
     laps_df, points_df = get_dataframes(tcx_file)
+    points_df['time_short']=(points_df.time.dt.strftime('%Y-%m-%d %H:%M:%S'))
 
-
-    st.header('Uploaded file:')
+    '## Original file'
     st.metric('# Waypoints', '{:,}'.format(points_df.size))
     'First 5 waypoints:'
     st.table(points_df.head())
 
-    'Map:'
 
     max_lon=points_df.longitude.max()
     max_lat=points_df.latitude.max()
     min_lon=points_df.longitude.min()
     min_lat=points_df.latitude.min()
-    
-    max_lon
-    max_lat
-    min_lon
-    min_lat
 
     max_bound = max(abs(max_lon-min_lon), abs(max_lat-min_lat)) * 111
-    zoom = 12.8 - np.log(max_bound)
-
+    zoom = 13 - np.log(max_bound)
     fig=px.scatter_mapbox(points_df,lat='latitude', lon='longitude', zoom=zoom, hover_data={'time'})
-    fig.update_layout(mapbox_style="stamen-toner")
+    fig.update_layout(mapbox_style="stamen-toner",margin={'b':0,'l':0,'r':0,'l':0})
     fig.update_traces(marker=dict(size=5))
     st.plotly_chart(fig)
 
@@ -48,7 +44,7 @@ if tcx_file is not None:
 
     
 
-    #REMOVE annoying crap
+    #Remove annoying fields that cause strava to miscalculate pace after editing
     all_speeds=soup.find_all('ns3:Speed')
     for speed in all_speeds:
         speed.decompose()
@@ -59,46 +55,62 @@ if tcx_file is not None:
     for dist in all_distances:
         dist.decompose()
 
+    error_start_time_slider=st.select_slider('Start time', points_df.time_short)
+    error_end_time_slider=st.select_slider('End time', points_df.time_short)
+
+
+
+    fig.add_trace(go.Scattermapbox(name='Error start', mode='markers',lat=points_df.latitude[0:1], lon=points_df.longitude[0:1] ,marker={'color':'green','size':30}))
+    
     #rename so strava doesnt think its a duplicate
     soup.find('Id').string.replace_with(str(random.randint(1000,10000)))
 
     if soup is not None:
-        'Use the chart above to find where the gpx track becomes very jumpy.'
-        'Find the beginning and the end of the bad areas, and record the times below.'
-        'We will interpolate these areas, assuming a constant speed between the beginning and end of the segments.'
 
-        st.metric('Start time:', (datetime.strptime(str(points_df.time.iloc[0]),'%Y-%m-%d %H:%M:%S%z')).strftime('%Y-%m-%dT%H:%M:%SZ'))
-        st.metric('End time:', (datetime.strptime(str(points_df.time.iloc[-1]),'%Y-%m-%d %H:%M:%S%z')).strftime('%Y-%m-%dT%H:%M:%SZ'))
-  
+        
+        '### Editing'
 
+        'Use the chart above to find areas where the gpx track is erroneous. Record the start and end of the jumpy section with the sliders. A "constant-speed-straight-line" will be drawn between the start point and end point.'
+        'Use the &larr; &rarr; keys for fine adjustments.'
+        
+        '_All times are in UTC_'
 
-        'Enter times in the format'
-        #error_start_time=st.text_input('Start time')
-        #error_end_time=st.text_input('End time', )
+        error_start_time=((datetime.strptime(str(error_start_time_slider),'%Y-%m-%d %H:%M:%S')).strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]+'Z')
+        error_end_time=((datetime.strptime(str(error_end_time_slider),'%Y-%m-%d %H:%M:%S')).strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]+'Z')
 
-        error_start_time_slider=st.select_slider('Start time', points_df.time)
-        error_end_time_slider=st.select_slider('End time', points_df.time)
+        interpolate_trackpoints(soup,error_start_time,error_end_time)
 
-        error_start_time=((datetime.strptime(str(error_start_time_slider),'%Y-%m-%d %H:%M:%S%z')).strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]+'Z')
-        error_end_time=((datetime.strptime(str(error_end_time_slider),'%Y-%m-%d %H:%M:%S%z')).strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]+'Z')
-
-        if error_end_time is not '':
-            st.metric('Start time', str(error_start_time))
-            st.metric('End time', str(error_end_time))
-
-            interpolate_trackpoints(soup,error_start_time,error_end_time)
-
+        if error_end_time_slider>error_start_time_slider:
+            
+            '## Edited file'
 
             with open("fixed.tcx", "w") as fixed_tcx_file:
                 fixed_tcx_file.write(str(soup))
 
                 laps_df_fixed, points_df_fixed = get_dataframes("fixed.tcx")
+                points_df_fixed['time_short']=(points_df_fixed.time.dt.strftime('%Y-%m-%d %H:%M:%S'))
 
-                fig2=px.scatter_mapbox(points_df_fixed,lat='latitude', lon='longitude', zoom=zoom, hover_data={'time'})
-                fig2.update_layout(mapbox_style="stamen-toner")
-                fig2.update_traces(marker=dict(size=3))
+                fig2=px.scatter_mapbox(points_df,lat='latitude', lon='longitude', zoom=zoom, hover_data={'time'})
+                fig2.update_layout(mapbox_style="stamen-toner",margin={'b':0,'l':0,'r':0,'l':0})
+                fig2.update_traces(marker=dict(size=5, opacity=1))
+                fig2.data[-1].name = 'Original route'
+                fig2.data[-1].showlegend = True
+                fig2.update_layout(legend=dict(yanchor="top",y=0.99,xanchor="right",x=0.99, bordercolor="Black",borderwidth=0.5))
+
+                fig2.add_trace(go.Scattermapbox(name='Edited route', mode='markers+text',lat=points_df_fixed.latitude, lon=points_df_fixed.longitude, hovertext=points_df_fixed.time ,marker={'color':'#FF4B4B','size':3}))
+
+
+                fig2.add_trace(go.Scattermapbox(name='Error start', mode='markers+text',lat=points_df_fixed.latitude[points_df_fixed.time_short==error_start_time_slider], lon=points_df_fixed.longitude[points_df_fixed.time_short==error_start_time_slider] ,marker={'color':'#2ca02c','size':10}))
+                fig2.add_trace(go.Scattermapbox(name='Error end', mode='markers+text',lat=points_df_fixed.latitude[points_df_fixed.time_short==error_end_time_slider], lon=points_df_fixed.longitude[points_df_fixed.time_short==error_end_time_slider] ,marker={'color':'#2ca02c','size':10}))
                 st.plotly_chart(fig2)
 
+
             with open("fixed.tcx", "r") as fixed_tcx_file:
-                    st.download_button('Download fixed tcx', fixed_tcx_file, file_name='fixed.tcx')
+                    st.download_button('Download fixed .tcx file', fixed_tcx_file, file_name='fixed.tcx')
                     print('done!')
+
+            'Upload your fixed track [here](https://www.strava.com/upload/select)'
+
+            '### Known Issues'
+            'If you already have a version of the activity uploaded to Strava, it may not upload the new one as it thinks it is a duplicate. You can fix this by deleting the original from Strava (you can always get the original back from Garmin Connect if needed)'
+            
